7cc8ad810169dafc90689f923623b8214a8e1bb4
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-04-02 09:34:45 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/jmwsw/',
-    bare:'https://math-english-sci.vercel.app/',
+    bare:'https://bare.alegithomeworksite.online/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
